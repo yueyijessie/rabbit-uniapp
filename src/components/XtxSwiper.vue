a5d1567e56bd83fb92ab3da52b0bd1991f5f0467
yueyijessie <welhomejessie@gmail.com>
2024-01-06 14:15:58 +0800

    从数据库获取轮播图数据并显示

--- src/components/XtxSwiper.vue
+++ src/components/XtxSwiper.vue
@@ -1,50 +1,33 @@
 <script setup lang="ts">
+import type { BannerItem } from '@/types/home'
 import { ref } from 'vue'
 
 const activeIndex = ref(0)
 
-const onchange: UniHelper.SwiperOnChange = (event) => {
+const onChange: UniHelper.SwiperOnChange = (event) => {
   console.log(event.detail.current)
   activeIndex.value = event.detail!.current // !为非空断言,主观上排除掉undefined的情况
 }
+// 定义 props 接收
+defineProps<{
+  list: BannerItem[]
+}>()
 </script>
 
 <template>
   <view class="carousel">
-    <swiper :circular="true" :autoplay="false" :interval="3000" @change="onchange">
-      <swiper-item>
+    <swiper :circular="true" :autoplay="false" :interval="3000" @change="onChange">
+      <swiper-item v-for="item in list" :key="item.id">
         <navigator url="/pages/index/index" hover-class="none" class="navigator">
-          <image
-            mode="aspectFill"
-            class="image"
-            src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/slider_1.jpg"
-          ></image>
-        </navigator>
-      </swiper-item>
-      <swiper-item>
-        <navigator url="/pages/index/index" hover-class="none" class="navigator">
-          <image
-            mode="aspectFill"
-            class="image"
-            src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/slider_2.jpg"
-          ></image>
-        </navigator>
-      </swiper-item>
-      <swiper-item>
-        <navigator url="/pages/index/index" hover-class="none" class="navigator">
-          <image
-            mode="aspectFill"
-            class="image"
-            src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/slider_3.jpg"
-          ></image>
+          <image mode="aspectFill" class="image" :src="item.imgUrl"></image>
         </navigator>
       </swiper-item>
     </swiper>
     <!-- 指示点 -->
     <view class="indicator">
       <text
-        v-for="(item, index) in 3"
-        :key="item"
+        v-for="(item, index) in list"
+        :key="item.id"
         class="dot"
         :class="{ active: index === activeIndex }"
       ></text>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
+import { getHomeBannerAPI } from '@/services/home';
 import CustomNavBar from './components/CustomNavBar.vue'
+import { onLoad } from '@dcloudio/uni-app';
+import { ref } from 'vue';
+import type { BannerItem } from '@/types/home';
+
+const bannerList = ref<BannerItem[]>([])
+
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+onLoad(()=>{
+  getHomeBannerData()
+})
+
 </script>
 
 <template>
   <!-- 自定义导航 -->
   <CustomNavBar></CustomNavBar>
   <!-- 自定义轮播图 -->
-  <XtxSwiper></XtxSwiper>
+  <XtxSwiper :list="bannerList"></XtxSwiper>
 
   <view class="index">index lalalalala</view>
 </template>
